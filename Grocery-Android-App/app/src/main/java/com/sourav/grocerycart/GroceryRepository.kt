98c5b88994dbd0c5a7e163c47733108500925205
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-09-23 01:23:03 +0530

    Add files via upload

---
+++ Grocery-Android-App/app/src/main/java/com/sourav/grocerycart/GroceryRepository.kt
@@ -0,0 +1,11 @@
+package com.sourav.grocerycart
+
+import com.sourav.grocerycart.GroceryDatabase
+import com.sourav.grocerycart.GroceryItems
+
+class GroceryRepository(private val db: GroceryDatabase) {
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+
+    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
+}
---
+++ Grocery-Android-App/app/src/main/java/com/sourav/grocerycart/GroceryViewModel.kt
@@ -0,0 +1,17 @@
+package com.sourav.grocerycart
+
+import androidx.lifecycle.ViewModel
+import com.sourav.grocerycart.GroceryItems
+import com.sourav.grocerycart.GroceryRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class GroceryViewModel(private val repository: GroceryRepository):ViewModel() {
+    fun insert(items: GroceryItems) = GlobalScope.launch {
+        repository.insert(items)
+    }
+    fun delete(items: GroceryItems) = GlobalScope.launch {
+        repository.delete(items)
+    }
+    fun getAllGroceryItems() = repository.getAllItems()
+}
